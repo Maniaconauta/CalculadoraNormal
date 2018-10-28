d2db1149019cb6c4466797bef37eb5db2b366b85
Maniaconauta <44531774+Maniaconauta@users.noreply.github.com>
2018-10-27 21:50:50 -0500

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -1,10 +1,8 @@
 <?php
-
 echo "Calculator Application";
-
 ?>
 
 <br>
 
 The sume of two numbers <?php echo $_GET["num1"]; ?> and <?php echo $_GET["num2"]; ?> is <br>
-<?php echo $_GET["num1"]+$_GET["num2"]; ?> !
+<?php echo $_GET["num1"]+$_GET["num2"]; ?>
